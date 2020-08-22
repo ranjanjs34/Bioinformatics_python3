d795923b93b7e678c43a3c98c38552e4fc7fde14
Ranjan Jyoti Sarma <ranjanjsarma@gmail.com>
2020-08-23 00:55:14 +0530

    Create GC Plotting of Two Sequences

--- GC-Plotting_of_two_Sequences.py
+++ GC-Plotting_of_two_Sequences.py
@@ -1,10 +1,22 @@
+#importing Ploting modules
 import matplotlib
 import matplotlib.pyplot as plt
+#importing BioPython
 import Bio
 from Bio.Seq import Seq
-from collections import Counter
-dna=Seq('ATGCATGACAG')
-freq=Counter(dna)
-print (freq)
-plt.bar(freq.keys(), freq.values())
+
+dna1=Seq("ATGCAAGGACGATACGATTAGAC")
+
+dna2=Seq("AAATGGCCCCCCCCCGCTTCGCCCCCCCCCCTCGGCGCAAATAGCCCAGAGGACATTAG")
+
+gc1=dna1.count('G')+dna1.count('C')
+
+gc2=dna2.count('G')+dna2.count('C')
+
+gcfreq= {'Seq1':gc1, 'Seq2':gc2} 
+
+print (gcfreq)
+
+plt.bar(gcfreq.keys(), gcfreq.values())
+#it will show the GC plots in GTK3 window
 plt.show()
